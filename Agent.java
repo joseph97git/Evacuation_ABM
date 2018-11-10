ad1c0966db116a425a1fb180f5343393212ad14e
Joseph Kim <joseph97@vt.edu>
2018-11-10 18:12:55 -0500

    updated agent class

--- Agent.java
+++ Agent.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.geom.*;
-import java.util.ArrayList;
 
 /**
  * The agents within the model that
@@ -15,31 +14,367 @@ public class Agent extends JPanel {
      * Prevent different serial versions.
      */
     private static final long serialVersionUID = 2L;
-    private int id;
-    private double x;
-    private double y;
-    private double xTarget;
-    private double yTarget;
+
+    private static final double A = 2000.0; // N
+    private static final double B = 0.08; // m
+    private static final double tau = 0.5; // s
+    private static final double k = 120000.0; // kg * s^{-2}
+    private static final double kappa = 240000.0; // kg * m^{-1} * s^{-1}
+    private static final double vi = 0.8; // m * s^{-1}
+    private static final double delta_t = 0.01; // s
+
+    private int id; // identification
+    private double[] p; // position
+    private double[] target; // exit
     private double radius;
+    private double[] center;
     private double mass;
     private double[] velocity;
-    private Agent[] other;
-    private Ellipse2D.Double person;
+    private Agent[] other; // all agents
+    private Ellipse2D.Double person; // GUI representation
 
 
     /**
      * Creates a new agent object.
      */
-
-    public Agent(int id, double x, double y, double r, double m, double [] v, Agent[] other) {
+    public Agent(
+        int id,
+        double[] p,
+        double r,
+        double m,
+        double[] v,
+        Agent[] other) {
         this.id = id;
-        this.x = x;
-        this.y = y;
+        this.p = p;
         this.radius = r;
+        this.center = new double[2];
+        this.center[0] = this.p[0] + r;
+        this.center[1] = this.p[1] + r;
         this.mass = m;
         this.velocity = v;
         this.other = other;
-        this.person = new Ellipse2D.Double(x, y, r, r);
+        this.person = new Ellipse2D.Double(this.p[0], this.p[1], r, r);
+    }
+
+
+    /**
+     * Updates this agent's position
+     * and velocity using Euler forward
+     * model.
+     */
+    public void updateVectors() {
+        // velocity time-step
+        double[] vtime = new double[2];
+        vtime[0] = this.velocity[0] * delta_t;
+        vtime[1] = this.velocity[1] * delta_t;
+        // force time-step
+        double[] ftime = new double[2];
+        double[] force = this.f();
+        ftime[0] = force[0] * delta_t / this.mass;
+        ftime[1] = force[1] * delta_t / this.mass;
+        // position update
+        this.p[0] = this.p[0] + vtime[0];
+        this.p[1] = this.p[1] + vtime[1];
+        this.person.setFrame(this.p[0], this.p[1], this.radius, this.radius);
+        // velocity update
+        this.velocity[0] = this.velocity[0] + ftime[0];
+        this.velocity[1] = this.velocity[1] + ftime[1];
+    }
+
+
+    /**
+     * Calculates the distance between two agents'
+     * center of masses.
+     * 
+     * @param i
+     *            The first agent
+     * @param j
+     *            The second agent
+     * @return The distance between them.
+     */
+    public double dist_ij(double[] i, double[] j) {
+        double x1 = i[0];
+        double y1 = i[1];
+        double x2 = j[0];
+        double y2 = j[1];
+        double diffXsqrd = (x2 - x1) * (x2 - x1);
+        double diffYsqrd = (y2 - y1) * (y2 - y1);
+        return Math.sqrt(diffXsqrd + diffYsqrd);
+    }
+
+
+    /**
+     * Calculates the forces acting on the agent
+     * given a position vector and the current
+     * time step.
+     * 
+     * @return The resulting force vector.
+     */
+    public double[] f() {
+        double[] interactiveForce = new double[2]; // force btw agents
+        double[] wallForce = new double[2]; // force with walls
+        double[] agentForce = this.f_i(); // force to evacuate
+        interactiveForce[0] = 0;
+        interactiveForce[1] = 0;
+        wallForce[0] = 0;
+        wallForce[1] = 0;
+        // loop through the other agents
+        // compute inner-agent forces
+        for (int i = 0; i < this.other.length; i++) {
+            if (i != this.id) {
+                if (this.isTouching(this, this.other[i])) {
+                    double[] bf = this.f_ij(this, this.other[i]);
+                    interactiveForce[0] = interactiveForce[0] + bf[0];
+                    interactiveForce[1] = interactiveForce[1] + bf[1];
+                }
+            }
+        }
+        // loop through the surrounding walls
+        // compute wall-agent forces
+
+        // calculate force
+        double[] ftot = new double[2];
+        ftot[0] = agentForce[0] + interactiveForce[0];
+        ftot[1] = agentForce[1] + interactiveForce[1];
+        return ftot;
+    }
+
+
+    /**
+     * Calculates the force pushing the
+     * agent to the exit. Represents the
+     * agent's internal motivation to get
+     * to the nearest exit.
+     * 
+     * @return The motivation force vector.
+     */
+    public double[] f_i() {
+        double xsqrd = this.velocity[0] * this.velocity[0];
+        double ysqrd = this.velocity[1] * this.velocity[1];
+        double velNorm = Math.sqrt(xsqrd + ysqrd);
+        double[] zVec = new double[2];
+        zVec[0] = 0.0;
+        zVec[1] = 0.0;
+        if (velNorm < vi) {
+            double[] velNew = new double[2];
+            double[] velMax = new double[2];
+            double[] f = new double[2];
+            double[] ei = this.e_i();
+            velMax[0] = vi * ei[0];
+            velMax[1] = vi * ei[1];
+            velNew[0] = velMax[0] - this.velocity[0];
+            velNew[1] = velMax[1] - this.velocity[1];
+            f[0] = this.mass * (velNew[0] / tau);
+            f[1] = this.mass * (velNew[1] / tau);
+            return f;
+        }
+        return zVec;
+    }
+
+
+    /**
+     * Calculates the 'body force' and 'tangential force'
+     * between two agents.
+     * 
+     * @param i
+     *            The first agent
+     * @param j
+     *            The second agent
+     * @return The resulting body force vector.
+     */
+    public double[] f_ij(Agent i, Agent j) {
+        // base info
+        double r_ij = i.radius + j.radius;
+        double d_ij = this.dist_ij(i.center, j.center);
+        double[] n_ij = this.n_ij(j.center);
+        double[] v_ji = this.v_ji(j.velocity);
+        double[] t_ij = this.t_ij(n_ij);
+        // create scalars
+        double normal_scaler = A * Math.exp((r_ij - d_ij) / B) + k * g(r_ij
+            - d_ij);
+        double tangent_scaler = kappa * g(r_ij - d_ij) * this.delta_v_ji(v_ji,
+            t_ij);
+        // calculate force components
+        double[] bodyForce = new double[2];
+        bodyForce[0] = normal_scaler * n_ij[0];
+        bodyForce[1] = normal_scaler * n_ij[1];
+        double[] tangentForce = new double[2];
+        tangentForce[0] = tangent_scaler * t_ij[0];
+        tangentForce[1] = tangent_scaler * t_ij[1];
+        // calculate sum of forces
+        double[] f_ij = new double[2];
+        f_ij[0] = bodyForce[0] + tangentForce[0];
+        f_ij[1] = bodyForce[1] + tangentForce[1];
+        return f_ij;
+    }
+    
+    /**
+     * @param The wall giving force to Agent
+     * @return an x and y component of force vector
+     */
+    public double[]  wallForce(Wall wall) {
+        double[] result = new double[2];
+        double distance = Math.sqrt(Math.pow(wall.xCoord() - this.p[0], 2) + Math.pow(wall.yCoord() - this.p[1], 2));
+        if (distance < this.radius())
+        {
+
+        double xtComp = (wall.getX() - this.getX()) / distance;
+        double ytComp = (wall.getY() - this.getY()) / distance;
+        double[] tiW = new double[] {xtComp,ytComp}; //vector tangent of direction of wall
+        double[] niW = new double[] {ytComp,xtComp * -1}; //vector perpendicular of direction of wall
+        double A = 1.0; //constant
+        double B = 1.0 ; //constant
+        double k = 1.0; //constant
+        
+        result[0] = ((A*Math.exp(this.radius() - distance))/B + k*(this.radius()- distance))*niW[0];    //part1 x component 
+        result[1] = ((A*Math.exp(this.radius() - distance))/B + k*(this.radius()- distance))*niW[1];  //part1 y component
+        
+        double part2 = k*(this.radius() - distance)*(this.velocity()[0] * tiW[0] + this.velocity()[1] * tiW[1]); //part2 of formula
+        result[0] = result[0]+part2*tiW[0]; //combining x components
+        result[1] = result[1]+part2*tiW[1]; //combining y components
+        }
+        else
+        {
+            result[0] = 0;
+            result[1] = 0;
+        }
+
+        return result;  //returns {x,y} vector array
+        
+    }
+
+
+    /**
+     * Calculates the 'body force' and 'tangential force'
+     * between this agent and the walls.
+     * 
+     * @param i
+     *            This agent
+     * @param w
+     *            The wall
+     * @return The resulting force vector.
+     */
+    public double[] f_iW(Agent i, Object w) {
+        return null;
+    }
+
+
+    /**
+     * Calculates the direction to the
+     * target as a unit vector.
+     * 
+     * @return The resulting unit vector.
+     */
+    public double[] e_i() {
+        double xi = this.center[0];
+        double yi = this.center[1];
+        double xt = this.target[0];
+        double yt = this.target[1];
+        double dist_it = this.dist_ij(this.center, this.target);
+        double[] unit = new double[2];
+        unit[0] = Math.abs(xt - xi) / dist_it;
+        unit[1] = Math.abs(yt - yi) / dist_it;
+        return unit;
+    }
+
+
+    /**
+     * Gets the unit normal vector pointing from
+     * agent j to agent i (this agent)
+     * 
+     * @param j
+     *            The center of the other agent.
+     * @return The unit normal vector
+     */
+    public double[] n_ij(double[] j) {
+        double d_ij = this.dist_ij(this.center, j);
+        double xi = this.center[0];
+        double yi = this.center[1];
+        double xj = j[0];
+        double yj = j[1];
+        double[] unit = new double[2];
+        unit[0] = (xi - xj) / d_ij;
+        unit[1] = (yi - yj) / d_ij;
+        return unit;
+    }
+
+
+    /**
+     * Returns the unit tangential vector of this agent
+     * relative to the other agent.
+     * 
+     * @param n_ij
+     *            The unit normal vector pointing from
+     *            agent j to agent i (this agent).
+     * @return unit tangential vector
+     */
+    public double[] t_ij(double[] n_ij) {
+        double[] unit = new double[2];
+        unit[0] = -1 * n_ij[1];
+        unit[1] = n_ij[0];
+        return unit;
+    }
+
+
+    /**
+     * Calculates the velocity difference between
+     * agent i (this agent) and agent j.
+     * 
+     * @param v_j
+     *            The velocity vector of agent j.
+     * @return The velocity difference.
+     */
+    public double[] v_ji(double[] v_j) {
+        double v_xi = this.velocity[0];
+        double v_yi = this.velocity[1];
+        double[] v_result = new double[2];
+        v_result[0] = v_j[0] - v_xi;
+        v_result[1] = v_j[1] - v_yi;
+        return v_result;
+    }
+
+
+    /**
+     * Calculates the tangential velocity difference.
+     * 
+     * @param v_ji
+     *            The velocity difference.
+     * @param t_ij
+     *            The unit tangent vector.
+     * @return The tangential velocity difference.
+     */
+    public double delta_v_ji(double[] v_ji, double[] t_ij) {
+        return (v_ji[0] * t_ij[0]) + (v_ji[1] * t_ij[1]);
+    }
+
+
+    /**
+     * Returns 0 if the argument x (r_ij - d_ij) and
+     * returns x (r_ij - d_ij) otherwise.
+     * 
+     * @param x
+     *            r_ij - d_ij
+     * @return 0, if not touching. x, otherwise.
+     */
+    private double g(double x) {
+        if (x < 0) {
+            return 0.0;
+        }
+        return x;
+    }
+
+
+    /**
+     * Checks if two agents are touching.
+     * 
+     * @param a1
+     *            The first agent
+     * @param a2
+     *            The second agent
+     * @return True, if touching. False, if not.
+     */
+    public boolean isTouching(Agent a1, Agent a2) {
+        return dist_ij(a1.center, a2.center) <= (a1.radius + a2.radius);
     }
 
 
@@ -55,44 +390,40 @@ public class Agent extends JPanel {
 
 
     /**
-     * Gets the x coordinate.
+     * Gets the position of the agent.
      * 
-     * @return The x coordinate.
+     * @return The position
      */
-    public double xCoord() {
-        return this.x;
+    public double[] pos() {
+        return this.pos();
     }
 
 
     /**
-     * Gets the y coordinate.
-     * 
-     * @return The y coordinate.
+     * Sets the position of the agent
      */
-    public double yCoord() {
-        return this.y;
+    public void setPos(double[] pos) {
+        this.p = pos;
     }
 
 
     /**
-     * Gets the x coordinate
-     * of the destination.
+     * Gets the center of the agent.
      * 
-     * @return The x coordinate.
+     * @return The array with center coordinates.
      */
-    public double xTarget() {
-        return this.xTarget;
+    public double[] center() {
+        return this.center;
     }
 
 
     /**
-     * Gets the y coordinate
-     * of the destination.
+     * Gets the target coordinates.
      * 
-     * @return The y coordinate.
+     * @return The target coordinates.
      */
-    public double yTarget() {
-        return this.yTarget;
+    public double[] target() {
+        return this.target;
     }
 
 
@@ -127,63 +458,12 @@ public class Agent extends JPanel {
 
 
     /**
-     * Sets the x target coordinate
-     * to the specified integer.
+     * Sets the target coordinates
      * 
-     * @param x
-     *            The new x coordinate.
+     * @param target
+     *            The target coordinates
      */
-    public void setXTarget(int x) {
-        this.xTarget = x;
+    public void setTarget(double[] target) {
+        this.target = target;
     }
-
-
-    /**
-     * Sets the y target coordinate
-     * to the specified integer.
-     * 
-     * @param y
-     *            The new y coordinate.
-     */
-    public void setYTarget(int y) {
-        this.yTarget = y;
-    }
-    
-    
-    
-    /**
-     * @param The wall giving force to Agent
-     * @return an x and y component of force vector
-     */
-    public double[]  wallForce(Wall wall) {
-    	double[] result = new double[2];
-    	double distance = Math.sqrt(Math.pow(wall.xCoord() - this.xCoord(), 2) + Math.pow(wall.yCoord() - this.yCoord(), 2));
-    	if (distance < this.radius())
-    	{
-
-    	double xtComp = (wall.getX() - this.getX()) / distance;
-    	double ytComp = (wall.getY() - this.getY()) / distance;
-    	double[] tiW = new double[] {xtComp,ytComp}; //vector tangent of direction of wall
-    	double[] niW = new double[] {ytComp,xtComp * -1}; //vector perpendicular of direction of wall
-   	double A = 1.0; //constant
-    	double B = 1.0 ; //constant
-    	double k = 1.0; //constant
-    	
-    	result[0] = ((A*Math.exp(this.radius() - distance))/B + k*(this.radius()- distance))*niW[0];	//part1 x component	
-    	result[1] = ((A*Math.exp(this.radius() - distance))/B + k*(this.radius()- distance))*niW[1];  //part1 y component
-    	
-    	double part2 = k*(this.radius() - distance)*(this.velocity()[0] * tiW[0] + this.velocity()[1] * tiW[1]); //part2 of formula
-    	result[0] = result[0]+part2*tiW[0]; //combining x components
-    	result[1] = result[1]+part2*tiW[1]; //combining y components
-    	}
-    	else
-    	{
-    		result[0] = 0;
-    		result[1] = 0;
-    	}
-
-    	return result;  //returns {x,y} vector array
-    	
-    }
-    
 }
