c808790298b4463b0de4583ae18934a485a514ef
Danfeng Wang <Danfeng Wang@192.168.1.107>
2018-11-12 20:14:39 -0500

    Fixed proportions

--- Layout.java
+++ Layout.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
@@ -24,9 +25,9 @@ public class Layout extends JPanel implements ActionListener {
     private Agent[] agents;
     private int width;
     private int depth;
+    private JFrame window;
     private Timer timer;
-
-
+    
     /**
      * Creates a new SingleRoom object.
      * 
@@ -42,41 +43,40 @@ public class Layout extends JPanel implements ActionListener {
         this.depth = depth;
         this.timer = new Timer(5, this);
         agents = new Agent[2];
+        
         double[] v_init1 = new double[2];
         v_init1[0] = 0.0;
         v_init1[1] = 0.0;
+        
         double[] v_init2 = new double[2];
         v_init2[0] = 0.0;
         v_init2[1] = 0.0;
+        
         double[] pos1 = new double[2];
         pos1[0] = 200.0;
         pos1[1] = 200.0 - 100.0;
+        
         double[] pos2 = new double[2];
         pos2[0] = 260.0;
         pos2[1] = 200.0 - 100.0;
+        
         Agent agent1 = new Agent(0, pos1, 30.0, 80.0, v_init1, this.agents);
         Agent agent2 = new Agent(1, pos2, 30.0, 80.0, v_init2, this.agents);
-        double[] target = new double[2];
-        target[0] = 230.0;
-        target[1] = 435.0;
-        agent1.setTarget(target);
-        agent2.setTarget(target);
+        
         this.agents[0] = agent1;
         this.agents[1] = agent2;
     }
-
 
     /**
      * Runs the single room evacuation.
      */
     public void run() {
-        JFrame window = new JFrame("Evacuation ABM");
+        this.window = new JFrame("Evacuation ABM");
         window.add(this);
         window.setVisible(true);
         window.setSize(this.width, this.depth);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
 
     /**
      * Paints the environment and
@@ -110,20 +110,26 @@ public class Layout extends JPanel implements ActionListener {
      */
     public void create(Graphics2D graphics) {
         // single room
-        Rectangle2D room = new Rectangle2D.Double(25, 25, this.width - 60,
-            this.depth - 80);
+        Rectangle2D room = new Rectangle2D.Double(25, 25, this.window.getWidth() - 60,
+            this.window.getHeight() - 80);
         graphics.setPaint(Color.BLACK);
         graphics.draw(room);
-
+        
+        // exit
+        Exit exit1 = new Exit((this.window.getWidth()-50)/2, this.window.getHeight()-55, 50.0, 10.0);
+        graphics.setPaint(Color.GREEN);
+        graphics.fill(exit1.door());
+        
         // two agents
+        double[] target = new double[2];
+        target[0] = exit1.xCoord();
+        target[1] = exit1.yCoord();
+        this.agents[0].setTarget(target);
+        this.agents[1].setTarget(target);
+        
         graphics.setPaint(Color.BLUE);
         graphics.fill(this.agents[0].person());
         graphics.fill(this.agents[1].person());
-
-        // exit
-        Exit exit1 = new Exit(220.0, 445.0, 50.0, 10.0);
-        graphics.setPaint(Color.GREEN);
-        graphics.fill(exit1.door());
     }
 
 }
