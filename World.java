36404aa91647e3e145fb32091fb4939191898ab4
Danfeng Wang <dwang619@vt.edu>
2018-12-01 17:25:26 -0500

    merged with LayoutWallTest

--- World.java
+++ World.java
@@ -3,23 +3,20 @@ public class World {
 	public static void main(String[] args) {
 		// Classroom proportion width:depth = 1.2647:1
 		int depth = 800;
-<<<<<<< HEAD
 		double width;
 		
-		// simulation for one room
-		width = depth*1.2647 + 200;
-		Layout1Room oneRoom = new Layout1Room((int) width, depth);
-		oneRoom.run();
+//		// simulation for one room
+//		width = depth*1.2647 + 200;
+//		Layout1Room oneRoom = new Layout1Room((int) width, depth);
+//		oneRoom.run();
 		 
 //		// simulation for two rooms
 //		width = depth*1.2647;
 //		Layout2Room twoRoom = new Layout2Room((int) width, depth);
 //		twoRoom.run();
 		
-=======
-		double width = depth*1.2647;
+		width = depth*1.2647;
 		LayoutWallTest twoRoom = new LayoutWallTest((int) width, depth);
 		twoRoom.run();
->>>>>>> branch 'master' of https://github.com/joseph97git/Evacuation_ABM
 	} 
 }
