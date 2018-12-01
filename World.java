e93f2b614b738b30ba414e5e1f2e42ce3e68ff62
dwang619 <dwang619@vt.edu>
2018-12-01 14:10:12 -0500

    Corrected agent radius, created a separate class of layout for 1 room

--- World.java
+++ World.java
@@ -3,8 +3,17 @@ public class World {
 	public static void main(String[] args) {
 		// Classroom proportion width:depth = 1.2647:1
 		int depth = 800;
-		double width = depth*1.2647;
+		double width;
+		
+//		// simulation for one room
+//		width = depth*1.2647 + 200;
+//		Layout1Room oneRoom = new Layout1Room((int) width, depth);
+//		oneRoom.run();
+		
+		// simulation for two rooms
+		width = depth*1.2647;
 		Layout2Room twoRoom = new Layout2Room((int) width, depth);
 		twoRoom.run();
+		
 	} 
 }
